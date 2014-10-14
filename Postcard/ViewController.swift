ca7a2e449f237d50100e18d31c4cb315bf5c6e69
Dale Sinnott <Dale@Dales-imac.local>
2014-10-14 17:27:28 -0400

    first rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var toNameLabel: UILabel!
+    @IBOutlet weak var fromNameLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameToTextField: UITextField!
+    @IBOutlet weak var enterNameFromTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func mailButton(sender: UIButton) {
+        toNameLabel.hidden = false
+        toNameLabel.text = enterNameToTextField.text
+        toNameLabel.textColor = UIColor.redColor()
+        
+        fromNameLabel.hidden = false
+        fromNameLabel.text = enterNameFromTextField.text
+        fromNameLabel.textColor = UIColor.redColor()
+ 
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        enterNameToTextField.text = "To: "
+        enterNameToTextField.resignFirstResponder()
+        
+        enterNameFromTextField.text = "From: "
+        enterNameFromTextField.resignFirstResponder()
+        
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+        
+    }
 
 }
 
